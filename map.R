e6b709520a2c3c69f35ff575e6eedcac8808c51a
Thomas Rosendal <thomas.rosendal@sva.se>
2016-06-24 23:08:41 +0200

    changes

--- map.R
+++ map.R
@@ -1,33 +1,38 @@
 library(sp)
-library(rgdal)
 library(leaflet)
+library(jsonlite)
 geojson <- readLines("counties.geojson", warn = FALSE)
 temp <- paste(geojson, collapse = "\n")
 geojson <- fromJSON(temp, simplifyVector = FALSE)
-# Default styles for all features
-geojson$style = list(
-  weight = 1,
-  color = "#555555",
-  opacity = 1,
-  fillOpacity = 0.8
-)
-map <- leaflet()
-
-
-                                        # Gather GDP estimate from all countries
 id <- sapply(geojson$features, function(feat) {
   feat$id
 })
+geojson$style = list(
+  weight = 1,
+  color = "#555555",
+  opacity = 0.1,
+  fillOpacity = 0.8
+)
+geojson$features[[2]]$properties
 
-## Color by per-capita GDP using quantiles
 pal <- rainbow(n = length(id))
-# Add a properties$style list to each feature
-geojson$features <- lapply(geojson$features, function(feat) {
-  feat$properties$style<- list(
-    fillColor = pal[4]
-  )
-  feat
-})
+## for(i in seq_len(length(geojson$features))){
+##   geojson$features[[i]]$properties$style <- list(
+##     fillColor = pal[i]
+##   )
+## }
+for(i in seq_len(length(geojson$features))){
+  geojson$features[[i]]$properties$style$fillColor <- pal[i]
+}
+
+
+
+map <- leaflet()
+map <- addTiles(map)
+map <- addGeoJSON(map, geojson, weight = 0.5, fillOpacity = 0.05)
+map <- fitBounds(map, 10.8, 55.2, 24.2, 69.2)
+map
+
 
 # Add the now-styled GeoJSON object to the map
 map <- leaflet()
