78c56022c95c19509e37afd92a41ba1f272ce1e3
Thomas Rosendal <thomas.rosendal@sva.se>
2016-06-27 09:56:55 +0200

    Added NUTS codes

--- map.R
+++ map.R
@@ -3,26 +3,63 @@ library(jsonlite)
 geojson <- readLines("counties.geojson", warn = FALSE)
 temp <- paste(geojson, collapse = "\n")
 geojson <- fromJSON(temp, simplifyVector = FALSE)
-## Grab the id field from the map data
-id <- sapply(geojson$features, function(feat) {
-  feat$id
-})
-## Get the nutsid
-nutsid <- sapply(geojson$features, function(feat) {
-    feat$properties$NUTS_ID
-})
-## The results and manual colour series(6-character hex colors)
-df <- data.frame(id = id,
-                 nutsid = nutsid,
-                 result = c(0.1, 0.5, 2.3, 0, 0,
-                            0, 0, 0, 4.5, 0,
-                            1, 0, 0, 0, 2.1,
-                            0, 0, 0, 0, 0,
-                            0),
-                 color = substr(rainbow(21), 1, 7), stringsAsFactors = FALSE)
+NUTS <- data.frame(NUTS3 = c("SE110", "SE121", "SE122", "SE123",
+                             "SE124", "SE125", "SE211", "SE212",
+                             "SE213", "SE214", "SE221", "SE224",
+                             "SE231", "SE232", "SE311", "SE312",
+                             "SE313", "SE321", "SE322", "SE331",
+                             "SE332"),
+                   Swedish_name = c("Stockholms län", "Uppsala län",
+                                    "Södermanlands län",
+                                    "Östergötlands län", "Örebro län",
+                                    "Västmanlands län",
+                                    "Jönköpings län",
+                                    "Kronobergs län", "Kalmar län",
+                                    "Gotlands län", "Blekinge län",
+                                    "Skåne län", "Hallands län",
+                                    "Västra Götalands län",
+                                    "Värmlands län", "Dalarnas län",
+                                    "Gävleborgs län",
+                                    "Västernorrlands län",
+                                    "Jämtlands län",
+                                    "Västerbottens län",
+                                    "Norrbottens län"),
+                   English_name = c("Stockholm County",
+                                    "Uppsala County",
+                                    "Södermanland County",
+                                    "Östergötland County",
+                                    "Örebro County",
+                                    "Västmanland County",
+                                    "Jönköping County",
+                                    "Kronoberg County",
+                                    "Kalmar County", "Gotland County",
+                                    "Blekinge County", "Skåne County",
+                                    "Hallands County",
+                                    "Västra Götaland County",
+                                    "Värmland County",
+                                    "Dalarna County",
+                                    "Gävleborg County",
+                                    "Västernorrland County",
+                                    "Jämtland County",
+                                    "Västerbotten County",
+                                    "Norrbotten County"))
+##Add data and colours to this dataframe
+NUTS$result <- c(1, 2, 4, 1, 0,
+                 0, 0, 3, 4, 0,
+                 1, 0, 2, 0, 3,
+                 0, 0, 2, 0, 0,
+                 1)
+##Add the colour for each result value (Done manually to allow complete flexibility)
+cols2016 <- rev(c("#D22630", "#DD5431", "#E98132", "#F4AF32", "#FFDC33"))
+NUTS$colour <- NA
+NUTS$colour[NUTS$result == 0] <- cols2016[1]
+NUTS$colour[NUTS$result == 1] <- cols2016[2]
+NUTS$colour[NUTS$result == 2] <- cols2016[3]
+NUTS$colour[NUTS$result == 3] <- cols2016[4]
+NUTS$colour[NUTS$result == 4] <- cols2016[5]
 ## Add the result to the geojson (Not necessary)
 geojson$features <- lapply(geojson$features, function(feat){
-    feat$properties$result <- df$result[match(feat$id, df$id)]
+    feat$properties$result <- NUTS$result[match(feat$properties$NUTS_ID, NUTS$NUTS3)]
     feat
 })
 ## Style the map
@@ -35,7 +72,7 @@ geojson$style = list(
 ## Add the colour to the geojson
 geojson$features <- lapply(geojson$features, function(feat) {
   feat$properties$style <- list(
-    fillColor = df$color[match(feat$id, df$id)]
+    fillColor = NUTS$colour[match(feat$properties$NUTS_ID, NUTS$NUTS3)]
   )
   feat
 })
@@ -51,4 +88,10 @@ map <- addCircleMarkers(map,
                         fill = TRUE)
 map <- addTiles(map)
 map <- fitBounds(map, 10.8, 55.2, 24.2, 69.2)
+map <- addLegend(map,
+                 "bottomright",
+                 colors = cols2016,
+                 labels = c(0,1,2,3,4),
+                 title = "Number of Cases of XXXX per County",
+                 opacity = 1)
 map
