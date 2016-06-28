ce2fb0b3f9b67092d32154841e7e6ee779c7dc49
Thomas Rosendal <thomas.rosendal@sva.se>
2016-06-28 11:27:20 +0200

    Simplified mapping
    Saved a prepared geojson file that will be read into R and modified then
    written back to geojson and called from the html/javascript code

--- map.R
+++ map.R
@@ -1,58 +1,14 @@
 library(sp)
-library(svar)
 library(rgdal)
-data(NUTS_20M) 
-map_data <- NUTS_20M[NUTS_20M@data$STAT_LEVL_ == 3,]
-map_data <- spTransform(map_data, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-NUTS <- data.frame(NUTS3 = c("SE110", "SE121", "SE122", "SE123",
-                             "SE124", "SE125", "SE211", "SE212",
-                             "SE213", "SE214", "SE221", "SE224",
-                             "SE231", "SE232", "SE311", "SE312",
-                             "SE313", "SE321", "SE322", "SE331",
-                             "SE332"),
-                   Swedish_name = c("Stockholms län", "Uppsala län",
-                                    "Södermanlands län",
-                                    "Östergötlands län", "Örebro län",
-                                    "Västmanlands län",
-                                    "Jönköpings län",
-                                    "Kronobergs län", "Kalmar län",
-                                    "Gotlands län", "Blekinge län",
-                                    "Skåne län", "Hallands län",
-                                    "Västra Götalands län",
-                                    "Värmlands län", "Dalarnas län",
-                                    "Gävleborgs län",
-                                    "Västernorrlands län",
-                                    "Jämtlands län",
-                                    "Västerbottens län",
-                                    "Norrbottens län"),
-                   English_name = c("Stockholm County",
-                                    "Uppsala County",
-                                    "Södermanland County",
-                                    "Östergötland County",
-                                    "Örebro County",
-                                    "Västmanland County",
-                                    "Jönköping County",
-                                    "Kronoberg County",
-                                    "Kalmar County", "Gotland County",
-                                    "Blekinge County", "Skåne County",
-                                    "Hallands County",
-                                    "Västra Götaland County",
-                                    "Värmland County",
-                                    "Dalarna County",
-                                    "Gävleborg County",
-                                    "Västernorrland County",
-                                    "Jämtland County",
-                                    "Västerbotten County",
-                                    "Norrbotten County"),
-                   stringsAsFactors = FALSE)
-##Add data and colours to this dataframe
-NUTS$result <- c(0, 1, 0, 0, 0,
-                 0, 0, 2, 0, 2,
-                 0, 3, 0, 1, 0,
-                 0, 0, 4, 0, 4,
-                 0)
-map_data@data$Swedish_name <- NUTS$Swedish_name[match(NUTS$NUTS3, map_data$NUTS_ID)]
-map_data@data$English_name <- NUTS$English_name[match(NUTS$NUTS3, map_data$NUTS_ID)]
-##map_data@data$result <- NUTS$result[match(NUTS$NUTS3, map_data$NUTS_ID)]
-unlink("counties.geojson")
-writeOGR(map_data, "counties.geojson", layer = "main", driver = "GeoJSON", check_exists = FALSE)
+map_data <- readOGR("counties.geojson", layer = "OGRGeoJSON")
+## Change the data
+map_data@data$result <- c(0, 1, 0, 0, 0,
+                          0, 0, 2, 0, 2,
+                          0, 3, 0, 1, 0,
+                          0, 0, 4, 0, 4,
+                          0)
+temp <- tempfile()
+writeOGR(map_data, temp, layer = "main", driver = "GeoJSON", check_exists = FALSE)
+temp <- readLines(temp, warn = FALSE)
+temp <- paste(temp, collapse = "\n")
+writeLines(c("var CountyData = ", temp), "data.js")
