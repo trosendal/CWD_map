a1a5ec5a989fb141bca552e0e469404f00c9292d
Thomas Rosendal <thomas.rosendal@sva.se>
2016-06-28 14:16:51 +0200

    Moved data to separate files

--- map.R
+++ map.R
@@ -1,14 +1,27 @@
 library(sp)
 library(rgdal)
+## Get the map of counties
 map_data <- readOGR("counties.geojson", layer = "OGRGeoJSON")
-## Change the data
-map_data@data$result <- c(0, 1, 0, 0, 0,
-                          0, 0, 2, 0, 2,
-                          0, 3, 0, 1, 0,
-                          0, 0, 4, 0, 4,
-                          0)
+## Read in the result data
+results <- read.csv2("results.csv", stringsAsFactors = FALSE)
+## Merge the results into the spatial object
+map_data@data$result <- results$result[match(results$NUTS_ID, map_data@data$NUTS_ID)]
+## Write the spatial object as a geojson
 temp <- tempfile()
 writeOGR(map_data, temp, layer = "main", driver = "GeoJSON", check_exists = FALSE)
 temp <- readLines(temp, warn = FALSE)
 temp <- paste(temp, collapse = "\n")
 writeLines(c("var CountyData = ", temp), "data.js")
+##
+## Points on the map
+##
+df <- read.csv2("points.csv", stringsAsFactors = FALSE, dec = ".")
+df$date <- as.Date(df$date)
+pts <- SpatialPoints(cbind(df$long, df$lat))
+proj4string(pts) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+pts <- SpatialPointsDataFrame(pts, df)
+temp <- tempfile()
+writeOGR(pts, temp, layer = "main", driver = "GeoJSON", check_exists = FALSE)
+temp <- readLines(temp, warn = FALSE)
+temp <- paste(temp, collapse = "\n")
+writeLines(c("var PointsData = ", temp), "points.js")
