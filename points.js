99382ef9b999dac81738a27bde33ae3341cdecf7
Thomas Rosendal <thomas.rosendal@sva.se>
2016-06-28 12:21:03 +0200

    Added some points to the map

---
+++ points.js
@@ -0,0 +1,13 @@
+var points = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!",
+	"colour": "#000000"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [16, 63]
+    }
+};
